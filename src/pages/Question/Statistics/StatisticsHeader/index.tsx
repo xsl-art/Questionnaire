ab088ad05e30b2f913ca67b0135381ae3cfa14c0
mine-x <16288941+xslcom@user.noreply.gitee.com>
2026-08-21 21:34:27 +0800

    fix: 修复客户端url地址格式

--- src/pages/Question/Statistics/StatisticsHeader/index.tsx
+++ src/pages/Question/Statistics/StatisticsHeader/index.tsx
@@ -37,8 +37,7 @@ const StatisticsHeader: FC = () => {
     if (!isPublished) return null;
 
     // 拼接 url
-    const url = `${CLIENT_HOST}#/question/${id}`;
-    console.log('复制url', url);
+    const url = `${CLIENT_HOST}/#/question/${id}`;
 
     // 定义二维码组件
     const QRCodeElem = (
